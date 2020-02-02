ce88da1ce93f5f1c0058598162b79fb126f34dbf
sergiyhonta <sergeyhonta@gmail.com>
2020-02-02 22:06:54 +0200

    Restriction not to sell if price is lower than buy

--- demo.py
+++ demo.py
@@ -8,43 +8,43 @@ import pandas as pd
 
 
 def main():
-    companies = ['KO'
-        # , 'FB', 'AAPL', 'AMZN', 'NFLX', 'GOOG', 'MSFT'
-        ]
+    companies = ['FB', 'AAPL', 'AMZN', 'NFLX', 'GOOG', 'MSFT']
     for company in companies:
         get_analytics_for_company(company)
 
 
 def get_analytics_for_company(company):
     print('==========================')
-    quotes = load_market_data_with_np('historical_data/2years/' + company + '.csv')
-    df = pd.read_csv('historical_data/2years/' + company + '.csv')
+    quotes = load_market_data_with_np('historical_data/10years/' + company + '.csv')
+    df = pd.read_csv('historical_data/10years/' + company + '.csv')
     fig = go.Figure(data=[go.Candlestick(x=df['Date'],
                                          open=df['Open'],
                                          high=df['High'],
                                          low=df['Low'],
                                          close=df['Close'],
-                                         increasing_line_color= 'white',
-                                         decreasing_line_color= 'black')])
+                                         increasing_line_color='white',
+                                         decreasing_line_color='black')])
     fig.update_layout(
         title=company,
-        yaxis_title=company+' stocks price',
-        xaxis_title="End of green line - buy date. End of red line - sell date.",)
-    
+        yaxis_title=company + ' stocks price',
+        xaxis_title="End of green line - buy date. End of red line - sell date.", )
+
     money = 50_000
     stocks = 0
     initial_money = round(money + stocks * quotes[200].open_price, 2)
     print(company + ' initially: date ' + str(quotes[200].date) + ', money ' + str(initial_money) + '$')
     alreadyBought = False
     alreadySold = True
+    price_for_buy = 0
     for i in range(200, len(quotes) - 1, 1):
         begin, resistance_level = Solution.should_buy(quotes[0:i]) or (None, None)
-        if not alreadyBought and not(begin is None):
-            print(str(quotes[i].date) + " buy  " + company)
+        if not alreadyBought and not (begin is None):
+            #print(str(quotes[i].date) + " buy  " + company)
             alreadyBought = True
             alreadySold = False
             stocks = int(money / quotes[i].open_price)
             money -= stocks * quotes[i].open_price
+            price_for_buy = quotes[i].open_price
             if resistance_level is not None:
                 fig.add_shape(
                     # Line Diagonal
@@ -60,11 +60,12 @@ def get_analytics_for_company(company):
                         )
                     ))
         begin, support_level = Solution.should_sell(quotes[0:i]) or (None, None)
-        if not alreadySold and not (begin is None):
-            print(str(quotes[i].date) + " sell " + company)
+        if not alreadySold and not (begin is None) and (price_for_buy < quotes[i].close_price):
             alreadySold = True
             alreadyBought = False
             money += stocks * quotes[i].close_price
+
+            #print(str(quotes[i].date) + " sell " + company + " .money=" + str(money))
             stocks = 0
             if support_level is not None:
                 fig.add_shape(
