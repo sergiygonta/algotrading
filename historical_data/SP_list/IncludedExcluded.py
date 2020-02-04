3691c4b433159b5235d395040a8cdcd4fe695900
sergiyhonta <sergeyhonta@gmail.com>
2020-02-04 20:57:13 +0200

    Fix issue with file parsing

--- historical_data/SP_list/IncludedExcluded.py
+++ historical_data/SP_list/IncludedExcluded.py
@@ -4,43 +4,41 @@ from datetime import date, datetime
 
 class IncludedExcludedEvent(NamedTuple):
     date: date
-    included: str
-    excluded: str
+    included_company: str
+    excluded_company: str
 
 
-def load_included_excluded(file_name):
-    import numpy as np
-    date, included, excluded = np.loadtxt(
-        file_name,
-        delimiter=',',
-        usecols=(0, 1, 2),
-        unpack=True,
-        skiprows=1,
-        dtype='datetime64[D], str, str')
-
+def simple_load_companies(file_name):
+    import csv
     out = []
-
-    for date_val, included_val, excluded_val in \
-            zip(date, included, excluded):
-        out.append(IncludedExcludedEvent(
-            date=date_val,
-            included=included_val,
-            excluded=excluded_val)
-        )
-
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        out = []
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                print(f"reading line {line_count}")
+                out.append(IncludedExcludedEvent(
+                    date=datetime.strptime(row[0], '%Y-%m-%d').date(),
+                    included_company=str(row[1]),
+                    excluded_company=str(row[2]))
+                )
+                line_count += 1
     return out
 
 def main():
-    included_excluded_list = load_included_excluded("IncludedExcluded.csv")
+    included_excluded_list = simple_load_companies("IncludedExcluded.csv")
     included_dict = {}
     for event in included_excluded_list:
-        included_dict[str(event.included)]=str(event.date)
+        included_dict[str(event.included_company)]=str(event.date)
     for event in included_excluded_list:
-        if included_dict.__contains__(event.excluded):
-            #company,date_from,date_to
-            print(event.excluded+','+
-                  dict.get(event.excluded, '2000-01-01')+','+
-                  str(event.date))
+        #company,date_from,date_to
+        print(event.excluded_company+','+
+              included_dict.get(event.excluded_company, '2000-01-01')
+              +','+str(event.date))
 
 if __name__ == "__main__":
     main()
