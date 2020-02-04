7980533793e37b9d3e126dd644cfe517cebac768
sergiyhonta <sergeyhonta@gmail.com>
2020-02-04 09:37:09 +0200

    Fixed bug in Wiki parser

--- WikiParsers/sp_current.py
+++ WikiParsers/sp_current.py
@@ -6,7 +6,7 @@ sp_500_current = []
 # Get the html source
 html = wp.page("List of S&P 500 companies").html().encode("UTF-8")
 df = pd.read_html(html)[0]
-for t in df[0][1:]:
-    sp_500_current.append(t)
+for key, row in df.iterrows():
+    sp_500_current.append(row["Symbol"])
 
 print(len(sp_500_current), sp_500_current)
