beb691f60d58f716d6903ddf7156b4dfba00df06
sergiyhonta <sergeyhonta@gmail.com>
2020-02-20 23:47:41 +0200

    Improvements of structure

--- algorightm/AutoTrader.py
+++ algorightm/AutoTrader.py
@@ -3,25 +3,18 @@ from datetime import datetime, date
 from historical_data.Quote import simple_load_market_data, PATH_TO_HISTORICAL_DATA
 from algorightm.DecisionMaker import DecisionMaker
 
-import plotly.graph_objects as go
-import pandas as pd
 import os
 
 from utils.GraphicUtils import Colors, draw_candlestick_chart, Line
 
-
-
 initial_money = 50_000
-
 
 def main():
     companies = os.listdir(PATH_TO_HISTORICAL_DATA)
     companies.sort()
     total_earnings = 0
-    # for company in companies:
-    total_earnings += get_analytics_for_company('AAPL.csv')
-    # print('From 2009-04-07 to 2020-01-31 from ' + str(initial_money) + '$ earning ' + str(
-    #     round(total_earnings / len(companies), 2)) + '$')
+    for company in companies:
+        total_earnings += get_analytics_for_company(company)
 
 
 def get_analytics_for_company(company):
@@ -58,7 +51,7 @@ def get_analytics_for_company(company):
     final_money = round(money + stocks * quotes[len(quotes) - 1].close_price, 2)
     print('{0:<9} from '.format(company) + str(quotes[100].date) + ' and 50000$ to ' +
           str(quotes[len(quotes) - 1].date) + ' and ' + str(final_money) + '$')
-    draw_candlestick_chart(PATH_TO_HISTORICAL_DATA, company, lines, "End of green line - buy date. End of red line - sell date.")
+    #draw_candlestick_chart(PATH_TO_HISTORICAL_DATA, company, lines, "End of green line - buy date. End of red line - sell date.")
     return final_money - initial_money
 
 
