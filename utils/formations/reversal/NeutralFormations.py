a2622a4a36ccd9a9a1d2d2ee2423f678a69e71c2
sergiyhonta <sergeyhonta@gmail.com>
2020-02-27 02:32:00 +0200

    Neutral formations improved analysis

--- utils/formations/reversal/NeutralFormations.py
+++ utils/formations/reversal/NeutralFormations.py
@@ -10,10 +10,10 @@ class NeutralFormations:
                 quotes[0].low_price < body_bottom(quotes[0]) and \
                 (body_length(quotes[0]) == 0 or
                  (min(upper_shadow(quotes[0]), lower_shadow(quotes[0])) / body_length > 3)):
-            return 1
-        return 0
+            return True
+        return False
 
     # доджи 4 цен
     def isDoji4prices(quotes: List[Quote]):
-        return len(quotes) >= 1 and quotes[0].low_price == quotes[0].open_price == quotes[0].close_price == quotes[
-            0].high_price
+        return len(quotes) >= 1 and quotes[0].low_price == quotes[0].open_price == \
+               quotes[0].close_price == quotes[0].high_price
