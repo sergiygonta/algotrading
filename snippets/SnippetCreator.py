769bd20b4454556740e96b75489972ff9506a1fd
sergiyhonta <sergeyhonta@gmail.com>
2020-02-27 21:50:41 +0200

    Read quotes in modified way

--- snippets/SnippetCreator.py
+++ snippets/SnippetCreator.py
@@ -1,15 +1,13 @@
-from datetime import timedelta
-
-from historical_data.Quote import simple_load_market_data, PATH_TO_HISTORICAL_DATA
+from historical_data.Quote import PATH_TO_HISTORICAL_DATA
+from utils.FileUtils import simple_load_market_data
 from utils.GraphicUtils import draw_candlestick_chart, Colors, Line
 from utils.SnippetUtils import analyze_from, analyze_to, less_that_three_month_interval, clear_snippets_directories
 from snippets.SnippetDataAnalyzer import SnippetDataAnalyzer
 
+
 def main():
     # companies = os.listdir("../historical_data/SP")
-    companies = ['FB.csv'
-       # , 'AAPL.csv', 'AMZN.csv', 'NFLX.csv', 'GOOG.csv'
-                 ]
+    companies = ['FB.csv']
     companies.sort()
     for company in companies:
         create_snippets_for_company(company)
@@ -30,7 +28,9 @@ def create_snippets_for_company(company):
         if left_border is not None:
             lines.append(Line(quotes[left_border].date, quotes[left_border].close_price, quotes[right_border].date,
                               quotes[right_border].close_price, Colors(snippet_type.value).name))
-    draw_candlestick_chart(PATH_TO_HISTORICAL_DATA, company, lines, "After green = buy, after red = sell, after blue = hold")
+    draw_candlestick_chart(PATH_TO_HISTORICAL_DATA, company, lines,
+                           "After green = buy, after red = sell, after blue = hold")
+
 
 if __name__ == "__main__":
     main()
