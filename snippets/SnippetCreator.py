c4c13df47cf4d30a20be39354878ebb02401c7ca
sergiyhonta <sergeyhonta@gmail.com>
2020-02-29 02:46:35 +0200

    Add configurable parameters and static batch size

--- snippets/SnippetCreator.py
+++ snippets/SnippetCreator.py
@@ -2,17 +2,19 @@ import os
 
 from historical_data.Quote import PATH_TO_HISTORICAL_DATA
 from historical_data.SP_list.GicsSectors import get_gics_code_by_company
+from snippets.SnippetConfiguration import NUMBER_OF_ROWS_IN_SNIPPET_FILE
+from snippets.SnippetDataAnalyzer import SnippetDataAnalyzer
 from utils.FileUtils import simple_load_market_data
 from utils.GraphicUtils import draw_candlestick_chart, Colors, Line
-from utils.SnippetUtils import analyze_from, analyze_to, less_that_three_month_interval, clear_snippets_directories
-from snippets.SnippetDataAnalyzer import SnippetDataAnalyzer
+from utils.SnippetUtils import clear_snippets_directories, less_than_interval, analyze_to
 
-PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET = 10
+
 
 
 def main():
     clear_snippets_directories()
-    companies = os.listdir("../historical_data/SP")
+    # companies = os.listdir("../historical_data/SP")
+    companies = ['FB.csv']
     companies.sort()
     for company in companies:
         create_snippets_for_company(company[0:len(company) - 4])
@@ -22,16 +24,14 @@ def create_snippets_for_company(company):
     lines = []
     quotes = simple_load_market_data(PATH_TO_HISTORICAL_DATA + company)
     gics = get_gics_code_by_company(company)
-    if not quotes or less_that_three_month_interval(quotes[0].date, quotes[len(quotes) - 1].date):
+    if not quotes or less_than_interval(quotes[0].date, quotes[len(quotes) - 1].date):
         return
-    for i in range(analyze_from(quotes), analyze_to(quotes), 1):
-        left_border, right_border, snippet_type = SnippetDataAnalyzer.growing_snippet(quotes, i, gics) or (
-            None, None, None)
+    for i in range(NUMBER_OF_ROWS_IN_SNIPPET_FILE, analyze_to(quotes), 1):
+        left_border, right_border, snippet_type = SnippetDataAnalyzer.growing_snippet(quotes, i, gics) or (None, None, None)
         if left_border is not None:
             lines.append(Line(quotes[left_border].date, quotes[left_border].close_price, quotes[right_border].date,
                               quotes[right_border].close_price, Colors(snippet_type.value).name))
-        left_border, right_border, snippet_type = SnippetDataAnalyzer.falling_snippet(quotes, i, gics) or (
-            None, None, None)
+        left_border, right_border, snippet_type = SnippetDataAnalyzer.falling_snippet(quotes, i, gics) or (None, None, None)
         if left_border is not None:
             lines.append(Line(quotes[left_border].date, quotes[left_border].close_price, quotes[right_border].date,
                               quotes[right_border].close_price, Colors(snippet_type.value).name))
--- snippets/SnippetDataAnalyzer.py
+++ snippets/SnippetDataAnalyzer.py
@@ -1,11 +1,11 @@
 from typing import List
 from algorightm.DecisionMaker import DecisionMaker
 from historical_data.Quote import Quote
-from snippets.SnippetCreator import PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET
-from utils.SnippetUtils import create_3month_snippet, less_that_three_month_interval, SnippetTypes
+from snippets.SnippetConfiguration import PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET
+from utils.SnippetUtils import create_snippet, SnippetTypes, less_than_interval
+
 
 AMPLITUDE_OF_CHANGE = 1 + PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100
-
 
 class SnippetDataAnalyzer:
 
@@ -13,18 +13,18 @@ class SnippetDataAnalyzer:
         if DecisionMaker.should_buy(quotes[0:split_point]) is None:
             return None
         pointer = split_point
-        while less_that_three_month_interval(quotes[split_point].date, quotes[pointer].date):
+        while less_than_interval(quotes[split_point].date, quotes[pointer].date):
             if AMPLITUDE_OF_CHANGE < quotes[pointer].close_price / quotes[split_point].close_price:
-                return create_3month_snippet(quotes, split_point, SnippetTypes.buy, gics)
+                return create_snippet(quotes, split_point, SnippetTypes.buy, gics)
             pointer += 1
-        return create_3month_snippet(quotes, split_point, SnippetTypes.hold, gics)
+        return create_snippet(quotes, split_point, SnippetTypes.hold, gics)
 
     def falling_snippet(quotes: List[Quote], split_point: int, gics: int):
         if DecisionMaker.should_sell(quotes[0:split_point]) is None:
             return None
         pointer = split_point
-        while less_that_three_month_interval(quotes[split_point].date, quotes[pointer].date):
+        while less_than_interval(quotes[split_point].date, quotes[pointer].date):
             if quotes[split_point].close_price / quotes[pointer].close_price > AMPLITUDE_OF_CHANGE:
-                return create_3month_snippet(quotes, split_point, SnippetTypes.sell, gics)
+                return create_snippet(quotes, split_point, SnippetTypes.sell, gics)
             pointer += 1
-        return create_3month_snippet(quotes, split_point, SnippetTypes.hold, gics)
+        return create_snippet(quotes, split_point, SnippetTypes.hold, gics)
