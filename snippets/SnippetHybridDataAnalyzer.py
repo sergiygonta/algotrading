079d91b9d8c6665f96342ecb444bd2cfba7d5f38
sergiyhonta <sergeyhonta@gmail.com>
2020-02-29 20:45:30 +0200

    Split into hybrid and simple data analyser

--- snippets/SnippetHybridDataAnalyzer.py
+++ snippets/SnippetHybridDataAnalyzer.py
@@ -4,12 +4,14 @@ from historical_data.Quote import Quote
 from snippets.SnippetConfiguration import PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET
 from utils.SnippetUtils import create_snippet, SnippetTypes, less_than_interval
 
-AMPLITUDE_OF_CHANGE = 1 + PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100
 
+AMPLITUDE_OF_CHANGE = 1 + PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100
 
 class SnippetDataAnalyzer:
 
     def growing_snippet(quotes: List[Quote], split_point: int, gics: int, company: str):
+        if DecisionMaker.should_buy(quotes[0:split_point]) is None:
+            return None
         pointer = split_point
         while less_than_interval(quotes[split_point].date, quotes[pointer].date):
             if AMPLITUDE_OF_CHANGE < quotes[pointer].close_price / quotes[split_point].close_price:
@@ -18,6 +20,8 @@ class SnippetDataAnalyzer:
         return create_snippet(quotes, split_point, SnippetTypes.hold, gics, company)
 
     def falling_snippet(quotes: List[Quote], split_point: int, gics: int, company: str):
+        if DecisionMaker.should_sell(quotes[0:split_point]) is None:
+            return None
         pointer = split_point
         while less_than_interval(quotes[split_point].date, quotes[pointer].date):
             if quotes[split_point].close_price / quotes[pointer].close_price > AMPLITUDE_OF_CHANGE:
