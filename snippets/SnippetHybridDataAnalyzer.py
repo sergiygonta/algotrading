4d8868128cc3107d94a760f83e4ea8a45a44b94a
sergiyhonta <sergeyhonta@gmail.com>
2020-03-01 13:13:24 +0200

    Add wrong direction of graphic check

--- snippets/SnippetHybridDataAnalyzer.py
+++ snippets/SnippetHybridDataAnalyzer.py
@@ -1,10 +1,8 @@
 from typing import List
 from algorightm.DecisionMaker import DecisionMaker
-from historical_data.Quote import Quote
-from snippets.SnippetConfiguration import PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET
+from historical_data.Quote import Quote, is_red, is_green
+from snippets.SnippetConfiguration import PERCENT_OF_GROWTH_OR_FALL_AFTER_SNIPPET, PERCENT_OF_MAXIMUM_ALLOWED_ROLLBACK
 from utils.SnippetUtils import create_snippet, SnippetTypes, less_than_interval
-
-AMPLITUDE_OF_CHANGE = 1 + PERCENTAGE_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100
 
 
 class SnippetHybridDataAnalyzer:
@@ -13,8 +11,13 @@ class SnippetHybridDataAnalyzer:
         if DecisionMaker.should_buy(quotes[0:split_point]) is None:
             return None
         pointer = split_point
+        amplitude_of_growth = quotes[split_point].high_price * (1 + PERCENT_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100)
+        amplitude_of_rollback = quotes[split_point].low_price * (1 - PERCENT_OF_MAXIMUM_ALLOWED_ROLLBACK / 100)
         while less_than_interval(quotes[split_point].date, quotes[pointer].date):
-            if AMPLITUDE_OF_CHANGE < quotes[pointer].close_price / quotes[split_point].close_price:
+            # if graphic goes in the direction where we don't want
+            if quotes[pointer].low_price < amplitude_of_rollback and is_red(quotes[pointer]):
+                return create_snippet(quotes, split_point, SnippetTypes.hold, gics, company)
+            if quotes[pointer].close_price > amplitude_of_growth and is_green(quotes[pointer]):
                 return create_snippet(quotes, split_point, SnippetTypes.buy, gics, company)
             pointer += 1
         return create_snippet(quotes, split_point, SnippetTypes.hold, gics, company)
@@ -23,8 +26,13 @@ class SnippetHybridDataAnalyzer:
         if DecisionMaker.should_sell(quotes[0:split_point]) is None:
             return None
         pointer = split_point
+        amplitude_of_fall = quotes[split_point].low_price * (1 - PERCENT_OF_GROWTH_OR_FALL_AFTER_SNIPPET / 100)
+        amplitude_of_rollback = quotes[split_point].high_price * (1 + PERCENT_OF_MAXIMUM_ALLOWED_ROLLBACK / 100)
         while less_than_interval(quotes[split_point].date, quotes[pointer].date):
-            if quotes[split_point].close_price / quotes[pointer].close_price > AMPLITUDE_OF_CHANGE:
+            # if graphic goes in the direction where we don't want
+            if quotes[pointer].high_price > amplitude_of_rollback and is_green(quotes[pointer]):
+                return create_snippet(quotes, split_point, SnippetTypes.hold, gics, company)
+            if quotes[pointer].close_price < amplitude_of_fall and is_red(quotes[pointer]):
                 return create_snippet(quotes, split_point, SnippetTypes.sell, gics, company)
             pointer += 1
         return create_snippet(quotes, split_point, SnippetTypes.hold, gics, company)
