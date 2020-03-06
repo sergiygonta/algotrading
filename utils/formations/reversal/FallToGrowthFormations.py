e97d9befb52303dd95a35f29cb612b42752d5828
sergiyhonta <sergeyhonta@gmail.com>
2020-03-06 09:47:39 +0200

    Removed classes of formations

--- utils/formations/reversal/FallToGrowthFormations.py
+++ utils/formations/reversal/FallToGrowthFormations.py
@@ -4,67 +4,70 @@ from historical_data.Quote import Quote, lower_shadow, body_length, upper_shadow
     body_bottom
 
 
-class FallToGrowthFormations:
-
-    # молот
-    def isHammer(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_red(quotes[0]) and is_green(quotes[2]) and body_length(quotes[1]) != 0 and \
-                quotes[1].low_price < min(quotes[0].low_price, quotes[2].low_price) and \
-                lower_shadow(quotes[1]) / upper_shadow(quotes[1]) > 3:
-            ratio_shadow_to_body = lower_shadow(quotes[1]) / body_length(quotes[1])
-            if ratio_shadow_to_body > 2:
-                return confirm_fall_to_growth_trend_reversal(quotes, ratio_shadow_to_body)
+# молот
+def isHammer(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if is_red(quotes[0]) and is_green(quotes[2]) and body_length(quotes[1]) != 0 and \
+            quotes[1].low_price < min(quotes[0].low_price, quotes[2].low_price) and \
+            lower_shadow(quotes[1]) / upper_shadow(quotes[1]) > 3:
+        ratio_shadow_to_body = lower_shadow(quotes[1]) / body_length(quotes[1])
+        if ratio_shadow_to_body > 2:
+            return confirm_fall_to_growth_trend_reversal(quotes, ratio_shadow_to_body)
+    return 0
 
-    # бычье поглощение
-    def isBullTakeover(quotes: List[Quote]) -> float:
-        if len(quotes) >= 2 and is_red(quotes[0]) and is_green(quotes[1]) and \
-                quotes[0].open_price > quotes[1].open_price and body_length(quotes[0]) != 0:
-            ratio_between_candles = body_length(quotes[1]) / body_length(quotes[0])
-            if ratio_between_candles > 1.2:
-                return ratio_between_candles
-        return 0
 
-    # доджи стрекоза
-    def isDojiDragonfly(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if quotes[1].high_price == body_top and is_red(quotes[0]) and is_green(quotes[2]) and \
-                quotes[1].low_price < body_bottom(quotes[1]) < min(quotes[0].open_price, quotes[2].close_price) and \
-                (body_length == 0 or lower_shadow(quotes[1]) / body_length(quotes[1]) > 3):
-            return confirm_fall_to_growth_trend_reversal(quotes, 1)
-        return 0
+# бычье поглощение
+def isBullTakeover(quotes: List[Quote]) -> float:
+    if len(quotes) >= 2 and is_red(quotes[0]) and is_green(quotes[1]) and \
+            quotes[0].open_price > quotes[1].open_price and body_length(quotes[0]) != 0:
+        ratio_between_candles = body_length(quotes[1]) / body_length(quotes[0])
+        if ratio_between_candles > 1.2:
+            return ratio_between_candles
+    return 0
 
-    # бычья харами
-    def isBullHarami(quotes: List[Quote]) -> float:
-        if len(quotes) >= 2 and \
-                quotes[0].close_price < quotes[1].open_price < quotes[1].close_price < quotes[0].open_price:
-            return confirm_fall_to_growth_trend_reversal(quotes, 1)
-        return 0
 
-    # утренняя звезда дожи
-    def isDojiMorningStar(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_red(quotes[0]) and is_green(quotes[2]) and \
-                body_top(quotes[1]) < min(quotes[0].close_price, quotes[2].open_price) and \
-                (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
-                                                    upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
-            return confirm_fall_to_growth_trend_reversal(quotes, 2)
+# доджи стрекоза
+def isDojiDragonfly(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if quotes[1].high_price == body_top and is_red(quotes[0]) and is_green(quotes[2]) and \
+            quotes[1].low_price < body_bottom(quotes[1]) < min(quotes[0].open_price, quotes[2].close_price) and \
+            (body_length == 0 or lower_shadow(quotes[1]) / body_length(quotes[1]) > 3):
+        return confirm_fall_to_growth_trend_reversal(quotes, 1)
+    return 0
 
-    # брошенный младенец
-    def isAbandonedBaby(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_red(quotes[0]) and is_green(quotes[2]) and \
-                quotes[1].high_price < min(quotes[0].low_price, quotes[2].low_price) and \
-                (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
-                                                    upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
-            return confirm_fall_to_growth_trend_reversal(quotes, 5)
+
+# бычья харами
+def isBullHarami(quotes: List[Quote]) -> float:
+    if len(quotes) >= 2 and \
+            quotes[0].close_price < quotes[1].open_price < quotes[1].close_price < quotes[0].open_price:
+        return confirm_fall_to_growth_trend_reversal(quotes, 1)
+    return 0
+
+
+# утренняя звезда дожи
+def isDojiMorningStar(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if is_red(quotes[0]) and is_green(quotes[2]) and \
+            body_top(quotes[1]) < min(quotes[0].close_price, quotes[2].open_price) and \
+            (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
+                                                upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
+        return confirm_fall_to_growth_trend_reversal(quotes, 2)
+    return 0
+
+
+# брошенный младенец
+def isAbandonedBaby(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
+        return 0
+    if is_red(quotes[0]) and is_green(quotes[2]) and \
+            quotes[1].high_price < min(quotes[0].low_price, quotes[2].low_price) and \
+            (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
+                                                upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
+        return confirm_fall_to_growth_trend_reversal(quotes, 5)
+    return 0
 
 
 def confirm_fall_to_growth_trend_reversal(quotes: List[Quote], power_of_signal: float) -> float:
--- utils/formations/reversal/GrowthToFallFormations.py
+++ utils/formations/reversal/GrowthToFallFormations.py
@@ -4,67 +4,70 @@ from historical_data.Quote import Quote, lower_shadow, upper_shadow, body_length
     body_top
 
 
-class GrowthToFallFormations:
-
-    # повешенный
-    def isHanged(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_green(quotes[0]) and is_red(quotes[2]) and body_length(quotes[1]) != 0 and \
-                quotes[1].high_price > max(quotes[0].high_price, quotes[2].high_price) and \
-                (upper_shadow(quotes[1]) == 0 or lower_shadow(quotes[1]) / upper_shadow(quotes[1]) > 3):
-            ratio_shadow_to_body = lower_shadow(quotes[1]) / body_length(quotes[1])
-            if ratio_shadow_to_body > 2:
-                return confirm_growth_to_fall_trend_reversal(quotes, ratio_shadow_to_body)
+# повешенный
+def isHanged(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if is_green(quotes[0]) and is_red(quotes[2]) and body_length(quotes[1]) != 0 and \
+            quotes[1].high_price > max(quotes[0].high_price, quotes[2].high_price) and \
+            (upper_shadow(quotes[1]) == 0 or lower_shadow(quotes[1]) / upper_shadow(quotes[1]) > 3):
+        ratio_shadow_to_body = lower_shadow(quotes[1]) / body_length(quotes[1])
+        if ratio_shadow_to_body > 2:
+            return confirm_growth_to_fall_trend_reversal(quotes, ratio_shadow_to_body)
+    return 0
 
-    # медвежье поглощение
-    def isBearishTakeover(quotes: List[Quote]) -> float:
-        if len(quotes) >= 2 and is_green(quotes[0]) and is_red(quotes[1]) and \
-                quotes[0].open_price < quotes[1].open_price and body_length(quotes[0]) != 0:
-            ratio_between_candles = body_length(quotes[1]) / body_length(quotes[0])
-            if ratio_between_candles > 1.2:
-                return ratio_between_candles
-        return 0
 
-    # доджи надгробие
-    def isDojiGravestone(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if quotes[1].low_price == body_bottom(quotes[1]) and is_green(quotes[0]) and is_red(quotes[2]) and \
-                quotes[1].high_price > body_top(quotes[1]) > max(quotes[0].open_price, quotes[2].close_price) and \
-                (body_length == 0 or upper_shadow(quotes[1]) / body_length(quotes[1]) > 3):
-            return confirm_growth_to_fall_trend_reversal(quotes, 1)
-        return 0
+# медвежье поглощение
+def isBearishTakeover(quotes: List[Quote]) -> float:
+    if len(quotes) >= 2 and is_green(quotes[0]) and is_red(quotes[1]) and \
+            quotes[0].open_price < quotes[1].open_price and body_length(quotes[0]) != 0:
+        ratio_between_candles = body_length(quotes[1]) / body_length(quotes[0])
+        if ratio_between_candles > 1.2:
+            return ratio_between_candles
+    return 0
 
-    # медвежья харами
-    def isBearishHarami(quotes: List[Quote]):
-        if len(quotes) >= 2 and \
-                quotes[0].open_price < quotes[1].close_price < quotes[1].open_price < quotes[0].close_price:
-            return confirm_growth_to_fall_trend_reversal(quotes, 1)
-        return 0
 
-    # вечерняя звезда дожи
-    def isDojiEveningStar(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_green(quotes[0]) and is_red(quotes[2]) and \
-                body_bottom(quotes[1]) > max(quotes[0].close_price, quotes[2].open_price) and \
-                (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
-                                                    upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
-            return confirm_growth_to_fall_trend_reversal(quotes, 2)
+# доджи надгробие
+def isDojiGravestone(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if quotes[1].low_price == body_bottom(quotes[1]) and is_green(quotes[0]) and is_red(quotes[2]) and \
+            quotes[1].high_price > body_top(quotes[1]) > max(quotes[0].open_price, quotes[2].close_price) and \
+            (body_length == 0 or upper_shadow(quotes[1]) / body_length(quotes[1]) > 3):
+        return confirm_growth_to_fall_trend_reversal(quotes, 1)
+    return 0
 
-    # брошенный младенец
-    def isAbandonedBaby(quotes: List[Quote]) -> float:
-        if len(quotes) < 3:
-            return 0
-        if is_green(quotes[0]) and is_red(quotes[2]) and \
-                quotes[1].low_price > max(quotes[0].high_price, quotes[2].high_price) and \
-                (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
-                                                    upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
-            return confirm_growth_to_fall_trend_reversal(quotes, 5)
+
+# медвежья харами
+def isBearishHarami(quotes: List[Quote]):
+    if len(quotes) >= 2 and \
+            quotes[0].open_price < quotes[1].close_price < quotes[1].open_price < quotes[0].close_price:
+        return confirm_growth_to_fall_trend_reversal(quotes, 1)
+    return 0
+
+
+# вечерняя звезда дожи
+def isDojiEveningStar(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
         return 0
+    if is_green(quotes[0]) and is_red(quotes[2]) and \
+            body_bottom(quotes[1]) > max(quotes[0].close_price, quotes[2].open_price) and \
+            (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
+                                                upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
+        return confirm_growth_to_fall_trend_reversal(quotes, 2)
+    return 0
+
+
+# брошенный младенец
+def isAbandonedBaby(quotes: List[Quote]) -> float:
+    if len(quotes) < 3:
+        return 0
+    if is_green(quotes[0]) and is_red(quotes[2]) and \
+            quotes[1].low_price > max(quotes[0].high_price, quotes[2].high_price) and \
+            (body_length(quotes[1]) == 0 or min(lower_shadow(quotes[1]),
+                                                upper_shadow(quotes[1])) / body_length(quotes[1]) > 2):
+        return confirm_growth_to_fall_trend_reversal(quotes, 5)
+    return 0
 
 
 def confirm_growth_to_fall_trend_reversal(quotes: List[Quote], power_of_signal: float) -> float:
